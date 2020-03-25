2d82884c011af5dc69e619d58b58127d8276d3e4
Ben <spartanhooah@gmail.com>
2020-03-24 20:58:10 -0500

    feature: make getUsers reactive

--- src/main/java/guru/springframework/springrestclientexamples/controller/UserController.java
+++ src/main/java/guru/springframework/springrestclientexamples/controller/UserController.java
@@ -5,10 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.server.ServerWebExchange;
+
+import static org.apache.logging.log4j.util.Strings.isBlank;
 
 @Slf4j
 @Controller
@@ -21,21 +22,66 @@ public class UserController {
         return "index";
     }
 
+//    @PostMapping("/users")
+//    public String formPost(Model model, ServerWebExchange serverWebExchange) {
+//        MultiValueMap<String, String> map = serverWebExchange.getFormData().block();
+//
+//        Integer limit = Integer.valueOf(map.get("limit").get(0));
+//
+//        log.debug("Received limit value of " + limit);
+//
+//        if (limit == null || limit == 0) {
+//            log.debug("Setting limit to a default of 10");
+//            limit = 10;
+//        }
+//
+//        model.addAttribute("users", apiService.getUsers(limit));
+//
+//        return "userlist";
+//    }
+
+//    @PostMapping("/users")
+//    public String formPost(ServerWebExchange exchange, Model model) {
+//
+//        exchange.getFormData().subscribe(map -> {
+//            String limit = map.getFirst("limit");
+//
+//            log.debug("Received limit: [" + limit + "]");
+//            if(isBlank(limit)) {
+//                limit = "10";
+//            }
+//
+//            model.addAttribute("users", apiService.getUsers(Integer.valueOf(limit)));
+//        });
+//
+//        return "userlist";
+//    }
+
     @PostMapping("/users")
-    public String formPost(Model model, ServerWebExchange serverWebExchange) {
-        MultiValueMap<String, String> map = serverWebExchange.getFormData().block();
+    public String formPost(Model model, ServerWebExchange webExchange) {
+        model.addAttribute("users",
+                apiService.getUsers(webExchange
+                        .getFormData()
+                        .map(data -> {
+                            String limitInput = data.getFirst("limit");
+                            log.debug("Received Limit value: " + limitInput);
+                            int limit;
+                            try {
+                                assert limitInput != null;
+                                limit = Integer.parseInt(limitInput);
+                            } catch (NumberFormatException e) {
+                                limit = 0;
+                            }
 
-        Integer limit = Integer.valueOf(map.get("limit").get(0));
+                            //default if zero
+                            if (limit == 0) {
+                                log.debug("Setting limit to default of 10");
+                                limit = 10;
+                            }
 
-        log.debug("Received limit value of " + limit);
+                            return limit;
+                        })));
 
-        if (limit == null || limit == 0) {
-            log.debug("Setting limit to a default of 10");
-            limit = 10;
-        }
-
-        model.addAttribute("users", apiService.getUsers(limit));
-
-        return "userlist";
+        return "userList";
     }
 }
--- src/main/java/guru/springframework/springrestclientexamples/service/ApiServiceImpl.java
+++ src/main/java/guru/springframework/springrestclientexamples/service/ApiServiceImpl.java
@@ -2,21 +2,45 @@ package guru.springframework.springrestclientexamples.service;
 
 import guru.springframework.api.domain.User;
 import guru.springframework.api.domain.UserData;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 @Service
-@AllArgsConstructor
 public class ApiServiceImpl implements ApiService {
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
+    private final String apiUrl;
+
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String apiUrl) {
+        this.restTemplate = restTemplate;
+        this.apiUrl = apiUrl;
+    }
 
     @Override
     public List<User> getUsers(Integer limit) {
-        UserData userData = restTemplate.getForObject("https://private-anon-b9cf8a53f0-apifaketory.apiary-mock.com/api/user?limit=" + limit, UserData.class);
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder
+                .fromUriString(apiUrl)
+                .queryParam("limit", limit);
+        UserData userData = restTemplate.getForObject(uriBuilder.toUriString(), UserData.class);
 
         return userData.getData();
+    }
+
+    @Override
+    public Flux<User> getUsers(Mono<Integer> limit) {
+        return WebClient.create(apiUrl)
+                .get()
+                .uri(uriBuilder -> uriBuilder.queryParam("limit", limit.block()).build())
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .flatMap(response -> response.bodyToMono(UserData.class))
+                .flatMapIterable(UserData::getData);
     }
 }
